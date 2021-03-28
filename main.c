ffbe4659bd1739ef9ff1c6f02371998a5e623e4e
Ridick-ulous <lukasdeeken@outlook.com>
2021-03-28 13:08:11 +0200

    Created UAC_Firmware project

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * UAC_Firmware.c
+ *
+ * Created: 28.03.2021 13:04:58
+ * Author : lukas
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
